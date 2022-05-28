fc2bdda2ea425dd6b8ee94a1e10d5c3a2e04f1dc
ec965 <22567131+ec965@users.noreply.github.com>
2022-05-28 00:32:35 -0700

    Add doc comments

--- src/plugin.ts
+++ src/plugin.ts
@@ -11,26 +11,28 @@ export interface Options {
 /**
  * @param args.exclude - glob pattern of files to exclude
  * @param args.ext - file extensions, defaults to: `["*.ts", "*.js", "*.jsx", "*.tsx"]`
- * @param args.root - root of the project, defaults to `"src/"`
+ * @param args.root - root folder to look for files, defaults to: "src/"
  * @returns plugin
  */
 export function pluginUnused(opts: Options): Plugin {
   const usedFiles: string[] = [];
-  const {
-    root = "src/",
-    ext = ["*.ts", "*.js", "*.jsx", "*.tsx"],
-    exclude = [],
-  } = opts;
+  const { ext = ["*.ts", "*.js", "*.jsx", "*.tsx"], exclude = [] } = opts;
+
+  let config: Parameters<NonNullable<Plugin["configResolved"]>>[number];
 
   return {
     name: "@gatsbylabs/vite-plugin-unused",
     enforce: "post",
     apply: "build",
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
     load(id) {
       usedFiles.push(id);
       return null;
     },
     async buildEnd() {
+      const root = path.join(config.root, opts.root ?? "src/");
       const extensionGlobs = ext.map((ext) => path.join(root, "**", ext));
 
       const fgOpts = { dot: true };
@@ -50,6 +52,12 @@ export function pluginUnused(opts: Options): Plugin {
   };
 }
 
+/**
+ * Glob files and return full path
+ * @param patterns - glob pattern(s)
+ * @param opts - fast glob options
+ * @returns found files
+ */
 export async function globResolve(
   patterns: string | string[],
   opts: fg.Options
@@ -58,6 +66,11 @@ export async function globResolve(
   return result.map((fileName) => path.resolve(__dirname, fileName));
 }
 
+/**
+ * Filter all values from the excluded array from th all array
+ * @param all
+ * @param exclude
+ */
 export function filterExcluded<T>(all: T[], exclude: T[]) {
   const s = new Set(exclude);
   return all.filter((a) => !s.has(a));
